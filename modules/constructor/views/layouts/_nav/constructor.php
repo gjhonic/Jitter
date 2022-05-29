a8f875d072a79ac57e720585f805185153c5f1c6
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2022-05-29 21:55:32 +0800

    Комит

--- modules/constructor/views/layouts/_nav/constructor.php
+++ modules/constructor/views/layouts/_nav/constructor.php
@@ -1,7 +1,11 @@
 <?php
 
 use yii\helpers\Url;
+use app\modules\constructor;
 
 return [
-
+    [
+        'label' => Yii::t('app', 'Games'),
+        'url' => Url::to('/constructor/game')
+    ],
 ];
